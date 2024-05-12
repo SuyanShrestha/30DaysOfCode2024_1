fcd9a1068a619c8410b001cc9d8e8909d8c7bbd8
Suyan Shrestha <99606480+SuyanShrestha@users.noreply.github.com>
2024-05-12 13:21:39 +0545

    Add files via upload

---
+++ day13/src/screens/RecipeDetailScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RecipeDetailScreen = (props) => {
+    console.log(props.route.params);
+
+    // let item = props.route.param;
+}
+
+export default RecipeDetailScreen;
